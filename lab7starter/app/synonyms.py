0672b1561bef5a6a18a1ac590fb531514c148904
Nur Mohsin <follow.mohsin@gmail.com>
2020-03-10 02:05:30 +0600

    assingment 7 solve

--- lab7starter/app/synonyms.py
+++ lab7starter/app/synonyms.py
@@ -1,17 +1,45 @@
 def get_synonyms_from_files():
-    all_synonyms = {"family": ["class", "category", "folk", "kinsfolk", "fellowship"],
-                    "eat": ["consume", "use up", "deplete", "exhaust", "feed"],
-                    "house": ["household", "home", "firm", "mansion", "put up", "domiciliate"]}
-
     try:
-        count = 1
+        count = 1 #make a counter of each line
         with open("synonyms.txt") as file:
             lines = file.readlines()
-            for line in lines:
-                if not line:
-                    print("Error in line " + count + ":")
+            all_synonyms = {} #creating response variable of dict
+            for line in lines: # reading each line
+                if not line.strip(): # error if empty line
+                    print("Error in line " + str(count) + ":")
                 else:
-                    print(line)
+                    data = line.replace('\n', '') # every line has new line. replace it
+                    syn_val = data.split(",")
+                    syn_data = [p for p in syn_val if p]
+                    if len(syn_data) > 1:
+                        syn_new = []
+                        for k in syn_data: # making list of correct synonyms
+                            i = k.strip()
+                            if i:
+                                syn_new.append(i)
+                            else:
+                                print("Error in line " + str(count) + ": " + str(data))
+                                break
+                        for j in syn_new:
+                            if j not in all_synonyms.keys(): # checking already key named
+                                extact_data = syn_new 
+                                all_synonyms[j] = extact_data
+                                all_synonyms[j].extend(extact_data)
+                                my_data = all_synonyms[j]
+                                mylist = list(dict.fromkeys(my_data)) # remove duplicate list value
+                                if j in mylist:
+                                    mylist.remove(j) # remove key if in list
+                                all_synonyms[j] = mylist
+                            else:
+                                extact_data = syn_new
+                                all_synonyms[j].extend(extact_data)
+                                my_data = all_synonyms[j]
+                                mylist = list(dict.fromkeys(my_data))
+                                if j in mylist:
+                                    mylist.remove(j)
+                                all_synonyms[j] = mylist
+                    else:
+                        print("Error in line " + str(count) + ": " + str(data))
                 count += 1
             return all_synonyms
     except Exception as e:
